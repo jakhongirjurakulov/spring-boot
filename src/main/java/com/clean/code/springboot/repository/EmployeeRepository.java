7ff9b2d1512451bd893ce4d743700ed08080c3ee
jakhongirjurakulov <65897453+jakhongirjurakulov@users.noreply.github.com>
2023-03-24 18:42:34 +0500

    Add files via upload

---
+++ src/main/java/com/clean/code/springboot/repository/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.clean.code.SpringBoot.repository;
+
+import com.clean.code.SpringBoot.domain.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
