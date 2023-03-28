058f5d98d3ef3155a8998ec7548d228e9d9342df
jakhongirjurakulov <65897453+jakhongirjurakulov@users.noreply.github.com>
2023-03-28 16:19:49 +0500

    Update EmployeeRepository.java

--- src/main/java/com/clean/code/springboot/repository/EmployeeRepository.java
+++ src/main/java/com/clean/code/springboot/repository/EmployeeRepository.java
@@ -6,5 +6,26 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+  List<Employee> findByName(String name);
+      List<Employee> findByNameAndLastName(String name, String lastName);
 
+  //    2nd way of findByName
+      @Query("select e from Employee e where e.name = :name")
+      List<Employee> findByNameQuery(@Param("name") String name);
+
+  //    3rd way of findByName
+      @Query(value = "select * from employee e where e.name = :name", nativeQuery = true)
+      List<Employee> findByNameQueryNative(@Param("name") String name);
+
+      List<Employee> findAllByNameLike(String name);
+      List<Employee> findAllByNameStartingWith(String name);
+      List<Employee> findAllByNameEndingWith(String name);
+
+  //    2nd way of findAllByName
+      @Query("select e from Employee e where UPPER(e.name) like upper(concat('%', :name, '%'))")
+      List<Employee> findAllByNameLikeQuery(String name);
+
+  //    3rd way of findAllByName
+      @Query(value = "select * from employee e where e.name like %:name%", nativeQuery = true)
+      List<Employee> findAllByNameLikeNative(@Param("name") String name);
 }
