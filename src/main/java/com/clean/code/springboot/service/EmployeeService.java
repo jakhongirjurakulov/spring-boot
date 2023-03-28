70129060c2bd8d204c4d572a1ae298a0082b1d4a
jakhongirjurakulov <65897453+jakhongirjurakulov@users.noreply.github.com>
2023-03-28 16:20:34 +0500

    Update EmployeeService.java

--- src/main/java/com/clean/code/springboot/service/EmployeeService.java
+++ src/main/java/com/clean/code/springboot/service/EmployeeService.java
@@ -21,4 +21,37 @@ public class EmployeeService {
     public List<Employee> findAll() {
         return employeeRepository.findAll();
     }
+
+    public Employee findById(Long id) {
+        return employeeRepository.findById(id).get();
+    }
+
+    public List<Employee> findByName(String name) {
+        return employeeRepository.findByName(name);
+    }
+
+    public List<Employee> findByNameAndLastName(String name, String lastName) {
+        return employeeRepository.findByNameAndLastName(name, lastName);
+    }
+
+    public List<Employee> findAllByNameStartingWith(String name) {
+        return employeeRepository.findAllByNameStartingWith(name);
+    }
+
+    public List<Employee> findAllByNameLike(String name) {
+        return employeeRepository.findAllByNameLike(name);
+    }
+
+    public List<Employee> findAllByNameEndingWith(String name) {
+        return employeeRepository.findAllByNameEndingWith(name);
+    }
+
+    public List<Employee> findAllByNameLikeQuery(String name) {
+        return employeeRepository.findAllByNameLikeQuery(name);
+    }
+
+    public void delete(Long id) {
+        Employee employee = employeeRepository.getReferenceById(id);
+        employeeRepository.delete(employee);
+    }
 }
