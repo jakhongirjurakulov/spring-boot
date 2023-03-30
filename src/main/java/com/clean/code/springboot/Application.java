3ae8c7a4cbf0449d71841b5c0d94ec7dc7303f93
jakhongirjurakulov <65897453+jakhongirjurakulov@users.noreply.github.com>
2023-03-30 13:44:55 +0500

    Update Application.java

--- src/main/java/com/clean/code/springboot/Application.java
+++ src/main/java/com/clean/code/springboot/Application.java
@@ -7,5 +7,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		
+//	@Scheduled(fixedRate = 2000)
+//	public void startRate() {
+//		System.out.println("New startRate " + new Date());
+//	}
+
+	@Scheduled(initialDelay = 1000L, fixedDelay = 2000)
+	public void startDelay() {
+		System.out.println("New startDelay " + new Date());
+	}
+
+	@Scheduled(cron = "0 44 12 30 Mar Thu")
+	public void startCron() {
+		System.out.println("New startCron " + new Date());
+	}
 	}
 }
