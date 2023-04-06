b0459ce6425c16bbf1c208e669647bfabd5a904b
jakhongirjurakulov <65897453+jakhongirjurakulov@users.noreply.github.com>
2023-04-06 14:09:12 +0500

    Update FileStorageResource.java

--- src/main/java/com/clean/code/springboot/web/rest/FileStorageResource.java
+++ src/main/java/com/clean/code/springboot/web/rest/FileStorageResource.java
@@ -1,10 +1,17 @@
 package com.cleancode.SpringBoot.web.rest;
 
-import com.cleancode.SpringBoot.domain.FileStorage;
-import com.cleancode.SpringBoot.service.FileStorageService;
+import clean.code.Spring.Boot.domain.FileStorage;
+import clean.code.Spring.Boot.service.FileStorageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileUrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.net.MalformedURLException;
+import java.net.URLEncoder;
 
 @RestController
 @RequestMapping("/api")
@@ -19,5 +26,31 @@ public class FileStorageResource {
     public ResponseEntity upload(@RequestParam("file") MultipartFile multipartFile) {
         fileStorageService.save(multipartFile);
         return ResponseEntity.ok(multipartFile + "file saqlandi.");
+    }
+    
+     @GetMapping("/preview/{hashId}")
+    public ResponseEntity previewFile(@PathVariable String hashId) throws MalformedURLException {
+        FileStorage fileStorage = fileStorageService.findByHashId(hashId);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline; fileName=\"" + URLEncoder.encode(fileStorage.getName()))
+                .contentType(MediaType.parseMediaType(fileStorage.getContentType()))
+                .contentLength(fileStorage.getFileSize())
+                .body(new FileUrlResource(String.format("%s/%s", uploadFolder, fileStorage.getUploadPath())));
+    }
+
+    @GetMapping("/download/{hashId}")
+    public ResponseEntity downloadFile(@PathVariable String hashId) throws MalformedURLException {
+        FileStorage fileStorage = fileStorageService.findByHashId(hashId);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; fileName=\"" + URLEncoder.encode(fileStorage.getName()))
+                .contentType(MediaType.parseMediaType(fileStorage.getContentType()))
+                .contentLength(fileStorage.getFileSize())
+                .body(new FileUrlResource(String.format("%s/%s", uploadFolder, fileStorage.getUploadPath())));
+    }
+
+    @DeleteMapping("/delete/{hashId}")
+    public ResponseEntity delete(@PathVariable String hashId) {
+        fileStorageService.delete(hashId);
+        return ResponseEntity.ok("Fayl o'chirildi");
     }
 }
